df6f3ab908862a17d692d1ec33ecf2070b017dc0
rebek <math919191@gmail.com>
2025-08-22 15:27:39 -0600

    grandparent sex inference cleaned up

--- sex_inference_analysis.py
+++ sex_inference_analysis.py
@@ -5,7 +5,7 @@ from typing import TypedDict
 import pandas as pd
 
 from CREST_sex_inference import read_simmap
-from sex_inference_analysis_probabilities import calc_LOD, calc_probability_mf
+from sex_inference_analysis_probabilities import calc_probability_mf
 from utils import read_in_seg_file_as_df, is_beginning_or_end_of_chromosome
 
 COLUMN_NAMES = "sample_1", "sample_2", "chromosome", "start", "end", "IBD_type", "genetic_pos_start", "genetic_pos_end", "genetic_len"
@@ -20,7 +20,6 @@ column_type_casting = {
 
 
 def get_significant_crossovers_and_gaps(relatives_df, simmap):
-    columns = ["sample_1", "sample_2", "chromosome", "start", "end", "is_co", 'from_p1', 'from_p2', 'ambiguous']
 
     sig_co_and_gaps = []
 
@@ -34,10 +33,7 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                               'chromosome': curr_row['chromosome'],
                               'start': curr_row['start'],
                               'end': curr_row['start'],
-                              'is_co': True,
-                              'from_p1': False,
-                              'from_p2': False,
-                              'ambiguous': False
+                              'is_co': True
                               }
 
         ending_crossover = {'sample_1': curr_row['sample_1'],
@@ -45,10 +41,7 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                             'chromosome': curr_row['chromosome'],
                             'start': curr_row['end'],
                             'end': curr_row['end'],
-                            'is_co': True,
-                            'from_p1': False,
-                            'from_p2': False,
-                            'ambiguous': False
+                            'is_co': True
                             }
 
         seg_gap = {'sample_1': curr_row['sample_1'],
@@ -56,48 +49,26 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                    'chromosome': curr_row['chromosome'],
                    "start": curr_row['start'],
                    "end": curr_row['end'],
-                   'is_co': False,
-                   'from_p1': False,
-                   'from_p2': False,
-                   'ambiguous': False
+                   'is_co': False
                    }
 
         if curr_row['IBD_type'] == 'IBD2':
             continue
 
-        if curr_row['overlaps_p1'] and not curr_row['overlaps_p2']:
-
+        if curr_row['overlaps_p1']:
             if not seg_starts_chromosome:
-                starting_crossover['from_p1'] = True
                 sig_co_and_gaps.append(starting_crossover)
 
             if not seg_ends_chromosome:
-                ending_crossover['from_p1'] = True
                 sig_co_and_gaps.append(ending_crossover)
 
-            seg_gap['from_p1'] = True
-            sig_co_and_gaps.append(seg_gap)
-
-
-        elif not curr_row['overlaps_p1'] and curr_row['overlaps_p2']:
-
-            if not seg_starts_chromosome:
-                starting_crossover['from_p2'] = True
-                sig_co_and_gaps.append(starting_crossover)
-
-            if not seg_ends_chromosome:
-                ending_crossover['from_p2'] = True
-                sig_co_and_gaps.append(ending_crossover)
-
-            seg_gap['from_p2'] = True
             sig_co_and_gaps.append(seg_gap)
 
     return pd.DataFrame(sig_co_and_gaps)
 
 
 def overlaps(ibd_seg_1, ibd_seg_2):
-    ret = ibd_seg_1['start'] <= ibd_seg_2['end'] and ibd_seg_1['end'] >= ibd_seg_2['start']
-    return ret
+    return ibd_seg_1['start'] <= ibd_seg_2['end'] and ibd_seg_1['end'] >= ibd_seg_2['start']
 
 def seg_overlaps_with_other_ibd_df(seg, other_ibd_df):
     other_ibd_df = other_ibd_df[other_ibd_df['chromosome'] == seg['chromosome']]
@@ -111,6 +82,7 @@ def seg_overlaps_with_other_ibd_df(seg, other_ibd_df):
             return True
 
     return False
+
 
 def add_overlap_col_to_ibd_seg_df(ibd_seg_df, ibd_segs_other, overlaps_col_name='overlaps'):
     # sort the rows by chromosome/start/end
@@ -128,19 +100,11 @@ def add_overlap_col_to_ibd_seg_df(ibd_seg_df, ibd_segs_other, overlaps_col_name=
 
     return ibd_seg_df
 
-def get_ibd_segs_between_relatives(seg_df, relatives):
-    return seg_df[seg_df['sample_1'].isin(set(relatives)) & seg_df['sample_2'].isin(set(relatives))]
-
-def get_ibd_segs_shared(seg_df, set1_relatives, set2_relatives):
+def get_ibd_segs_between_relatives(seg_df, set1_relatives, set2_relatives):
     return seg_df[
-        seg_df['sample_1'].isin(set(set1_relatives)) & seg_df['sample_2'].isin(set(set2_relatives)) |
-        seg_df['sample_1'].isin(set(set2_relatives)) & seg_df['sample_2'].isin(set(set1_relatives))
+        (seg_df['sample_1'].isin(set(set1_relatives)) & seg_df['sample_2'].isin(set(set2_relatives))) |
+        (seg_df['sample_1'].isin(set(set2_relatives)) & seg_df['sample_2'].isin(set(set1_relatives)))
       ]
-
-
-def get_ibd_segs_with_relatives(seg_df, relatives):
-    return seg_df[seg_df['sample_1'].isin(set(relatives)) | seg_df['sample_2'].isin(set(relatives))]
-
 
 def get_all_relative_ids(pedigree_id, seg_df):
     s1_ids = seg_df.loc[seg_df['sample_1'].str.startswith(f'{pedigree_id}_'), 'sample_1'].tolist()
@@ -155,9 +119,8 @@ def determine_descendants_and_relatives(pedigree_id, seg_df):
 
     descendants = [f'{pedigree_id}_g5-b1-i1', f'{pedigree_id}_g5-b2-i1', f'{pedigree_id}_g5-b3-i1']
     p1_relatives = list(set(all_relative_ids) - set(descendants))
-    p2_relatives = []
 
-    return descendants, p1_relatives, p2_relatives
+    return descendants, p1_relatives
 
 
 def write_df_output(results, output_file):
@@ -188,28 +151,25 @@ def main(
 
     for pedigree_id in pedigree_ids:
 
-        descendants_ids, p1_relatives_ids, p2_relatives_ids = determine_descendants_and_relatives(pedigree_id, seg_df)
+        descendants_ids, p1_relatives_ids = determine_descendants_and_relatives(pedigree_id, seg_df)
 
-        descendants_df = get_ibd_segs_between_relatives(seg_df, descendants_ids)
+        pedigree_df = seg_df[seg_df['sample_1'].str.contains(fr'^{pedigree_id}')]
 
-        descendants_and_p1_relatives_df = get_ibd_segs_shared(seg_df, descendants_ids, p1_relatives_ids)
-        descendants_and_p2_relatives_df = get_ibd_segs_shared(seg_df, descendants_ids, p2_relatives_ids)
+        descendants_df = get_ibd_segs_between_relatives(pedigree_df, descendants_ids, descendants_ids)
+
+        descendants_and_p1_relatives_df = get_ibd_segs_between_relatives(pedigree_df, descendants_ids, p1_relatives_ids)
 
         descendants_df = add_overlap_col_to_ibd_seg_df(descendants_df, descendants_and_p1_relatives_df, overlaps_col_name='overlaps_p1')
-        descendants_df = add_overlap_col_to_ibd_seg_df(descendants_df, descendants_and_p2_relatives_df, overlaps_col_name='overlaps_p2')
 
         sig_co_and_gaps_df = get_significant_crossovers_and_gaps(descendants_df, simmap=simmap)
 
-        print(sig_co_and_gaps_df)
         df_results.append(sig_co_and_gaps_df)
 
         print(pedigree_id)
 
     df_results = pd.concat(df_results, ignore_index=True)
 
-    output_file_name = output_file
-
-    write_df_output(df_results, output_file_name)
+    write_df_output(df_results, output_file)
 
 class AnalysisParams(TypedDict):
     window: int
@@ -225,7 +185,7 @@ def read_in_results_file_as_df(file):
 def get_pedigree_id_from_file(file):
     return file.split('_')[-1].split(".")[0]
 
-def do_data_analysis(input_file, data_analysis_params_lists, output_file, simmap):
+def do_data_analysis(input_file, window_size, output_file, simmap):
 
     simmap = read_simmap(simmap)
 
@@ -241,82 +201,21 @@ def do_data_analysis(input_file, data_analysis_params_lists, output_file, simmap
         relative_ids = get_all_relative_ids(pedigree_id, pedigrees_df)
         pedigree_df = pedigrees_df[pedigrees_df['sample_1'].isin(relative_ids) | pedigrees_df['sample_2'].isin(relative_ids)]
 
+        # drop duplicates?
+        # pedigree_df = pedigree_df.drop_duplicates(subset=["chromosome", "start", "end",	"is_co"])
+
         pedigree_data_analysis_result = {
             "pedigree_id": pedigree_id,
         }
 
-
-        # for index, params in enumerate(data_analysis_params_lists):
-        #
-        #     sig_co_gaps_df_copy = pedigree_df.copy()
-        #
-        #     sig_gaps_df = sig_co_gaps_df_copy[sig_co_gaps_df_copy['is_co'] == False].to_dict(orient="records")
-        #     sig_co_df = sig_co_gaps_df_copy[sig_co_gaps_df_copy['is_co'] == True].to_dict(orient="records")
-        #     # if params['remove_dups']:
-        #     #     sig_co_gaps_df_copy = remove_dups(sig_co_gaps_df_copy)
-        #
-        #     if not params['count_p1']:
-        #         # remove anyone that has p1
-        #         sig_co_gaps_df_copy = sig_co_gaps_df_copy[sig_co_gaps_df_copy['from_p1'] == False]
-        #
-        #     if not params['count_p2']:
-        #         # remove anyone that has p2
-        #         sig_co_gaps_df_copy = sig_co_gaps_df_copy[sig_co_gaps_df_copy['from_p2'] == False]
-        #
-        #     if not params['count_ambiguous_co']:
-        #         # remove any ambiguous crossovers
-        #         sig_co_gaps_df_copy = sig_co_gaps_df_copy[sig_co_gaps_df_copy['ambiguous'] == False]
-
-        index = 0
-        params = data_analysis_params_lists[0]
         sig_gaps_df = pedigree_df[pedigree_df['is_co'] == False].to_dict(orient="records")
         sig_co_df = pedigree_df[pedigree_df['is_co'] == True].to_dict(orient="records")
 
-        LOD = calc_probability_mf(sig_gaps_df, sig_co_df, simmap, window_size=params['window'])
-        pedigree_data_analysis_result[f'LOD_{index}'] = LOD
+        LOD = calc_probability_mf(sig_co_df, sig_gaps_df, simmap, window_size=window_size)
+        pedigree_data_analysis_result[f'LOD'] = LOD
 
         data_analysis_results.append(pedigree_data_analysis_result)
 
     results_df = pd.DataFrame(data_analysis_results)
 
     results_df.to_csv(output_file, sep='\t', index=False, header=True)
-
-
-# if __name__ == "__main__":
-#     import sys
-#
-#     # uncomment and modify to change the arguments here without using the command line
-#     # input_name = "sibs-cousin-M"
-#     # input_file = "../ped-sim/outputs/sibs-cousins-M.seg"
-#     # sys.argv = ['sex_inference_analysis.py',
-#     #             '--input', input_file,
-#     #             '--output', f'results/{input_name}_results.txt',
-#     #             '--simmap', '../ped-sim/refined_mf.simmap',
-#     #             '--count_ibd2_co', 'False'
-#     #             ]
-#
-#     parser = argparse.ArgumentParser()
-#
-#     parser.add_argument('-i', '--input', type=Path, help='Name of the segment input file')
-#     parser.add_argument('-o', '--output', type=Path, help='Name of the output file')
-#     parser.add_argument('-m', '--simmap', type=Path, help='Name of the genetic map file (should be in .simmap format)')
-#     parser.add_argument('-c', '--count_ambiguous_co', type=bool, help='True/False - count ambigious ibd2 crossovers')
-#
-#     # optional
-#     parser.add_argument('-b', '--bim',
-#                         help='A PLINK .bim containing the dataset-specific map (should contain 22 autosomes)')
-#     parser.add_argument('-w', '--window', metavar='window_size_in_kilobases',
-#                         type=int, default=500, help='Window size in kilobases. Default: 500 kb')
-#     parser.add_argument('-d', '--count_doubles',
-#                         type=bool, default='True', help='Count double crossovers')
-#
-#     args = parser.parse_args()
-#
-#     main(
-#         input_file=args.input,
-#         output_file=args.output,
-#         map_file=args.simmap,
-#         window=args.window,
-#         count_ambiguous_co=args.count_ambiguous_co,
-#         count_doubles=args.count_doubles
-#     )
--- sex_inference_analysis_probabilities.py
+++ sex_inference_analysis_probabilities.py
@@ -1,12 +1,4 @@
-from CREST_sex_inference import pos_search, logp_k_crossovers, logp_k_crossovers_for_length, log_product, logp_gaps, \
-    log_poisson
-
-
-def get_co_window(chromo, co_position, window_size, simmap):
-    gmap = simmap[str(chromo)]
-    gmap_ends = (gmap['pos'][0], gmap['pos'][-1])
-    # don't go beyond the usable ends
-    return max(co_position - (window_size / 2), gmap_ends[0]), min(co_position + (window_size / 2), gmap_ends[1])
+from CREST_sex_inference import pos_search, log_product, log_poisson
 
 
 def get_mf_lengths(chromo, start, end, simmap):
@@ -21,6 +13,12 @@ def get_mf_lengths(chromo, start, end, simmap):
     lengths['female'] = (end_cM_female - start_cM_female) / 100
 
     return lengths
+
+def get_co_window(chromo, co_position, window_size, simmap):
+    gmap = simmap[str(chromo)]
+    gmap_ends = (gmap['pos'][0], gmap['pos'][-1])
+    # don't go beyond the usable ends
+    return max(co_position - (window_size / 2), gmap_ends[0]), min(co_position + (window_size / 2), gmap_ends[1])
 
 
 def calc_significant_crossover_probability(significant_crossovers, simmap, window_size):
@@ -38,7 +36,7 @@ def calc_significant_crossover_probability(significant_crossovers, simmap, windo
     return list_logp_female, list_logp_male
 
 def get_gap_windows(sig_gap, window_size):
-    return sig_gap['start'], sig_gap['end']
+    return sig_gap['start'] + (window_size / 2), sig_gap['end'] - (window_size / 2)
 
 def calc_significant_gap_probability(significant_gaps, simmap, window_size):
     list_logp_female = []
@@ -69,52 +67,3 @@ def calc_probability_mf(significant_crossovers, significant_gaps, simmap, window
     LOD_all = logp_female - logp_male
 
     return LOD_all
-
-def calc_LOD(sig_gaps_df, sig_co_df, simmap, window_size):
-    list_logp_female_co, list_logp_male_co, female_lengths_co, male_lengths_co = calc_significant_crossover_probability(sig_co_df, simmap, window_size)
-    list_logp_female_gaps, list_logp_male_gaps, female_lengths_gaps, male_lengths_gaps = calc_significant_gap_probability(sig_gaps_df, simmap, window_size)
-
-    female_prob = log_product(list_logp_female_co + list_logp_female_gaps)
-    male_prob = log_product(list_logp_male_co + list_logp_male_gaps)
-
-    LOD = female_prob - male_prob
-    return LOD
-
-def calc_probability_using_both_parents(sig_crossovers_p1, sig_gaps_p1, sig_crossovers_p2, sig_gaps_p2, simmap, window, count_p1, count_p2):
-    co_prob_f_p1, co_prob_m_p1, female_lengths_co_p1, male_lengths_co_p1 = calc_significant_crossover_probability(sig_crossovers_p1, simmap, window)
-    gap_prob_f_p1, gap_prob_m_p1, female_lengths_gap_p1, male_lengths_gap_p1 = calc_significant_gap_probability(sig_gaps_p1, simmap, window)
-
-    co_prob_f_p2, co_prob_m_p2, female_lengths_co_p2, male_lengths_co_p2 = calc_significant_crossover_probability(sig_crossovers_p2, simmap, window)
-    gap_prob_f_p2, gap_prob_m_p2, female_lengths_gap_p2, male_lengths_gap_p2 = calc_significant_gap_probability(sig_gaps_p2, simmap, window)
-
-
-    lengths_p1f = female_lengths_co_p1 + male_lengths_co_p2
-    lengths_p1m = male_lengths_co_p1 + female_lengths_co_p2
-
-    LOD, co_LOD, gap_LOD = 0,0,0
-    if count_p1 and count_p2:
-        co_LOD = sum(co_prob_f_p1 + co_prob_m_p2) - sum(co_prob_m_p1 + co_prob_f_p2)
-        gap_LOD = sum(gap_prob_f_p1 + gap_prob_m_p2) - sum(gap_prob_m_p1 + gap_prob_f_p2)
-
-        # log product for p2 being female and p1 being male
-        logp_p1m_p2f = log_product(co_prob_m_p1 + gap_prob_m_p1 + co_prob_f_p2 + gap_prob_f_p2)
-
-        # log product for p1 being female and p2 being male
-        logp_p1f_p2m = log_product(co_prob_f_p1 + gap_prob_f_p1 + co_prob_m_p2 + gap_prob_m_p2)
-
-        LOD = logp_p1f_p2m - logp_p1m_p2f
-
-    elif count_p1 and not count_p2:
-        co_LOD = log_product(co_prob_f_p1) - log_product(co_prob_m_p1)
-        gap_LOD = log_product(gap_prob_f_p1) - log_product(gap_prob_m_p1)
-
-        LOD = co_LOD + gap_LOD
-
-    elif count_p2 and not count_p1:
-        co_LOD = log_product(co_prob_f_p2) - log_product(co_prob_m_p2)
-        gap_LOD = log_product(gap_prob_f_p2) - log_product(gap_prob_m_p2)
-
-        LOD = co_LOD + gap_LOD
-
-
-    return LOD, co_LOD, gap_LOD
