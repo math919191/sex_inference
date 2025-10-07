e89e96b49ea72c36946a7e2a0b9892a19a56d4ec
rebek <math919191@gmail.com>
2025-10-06 22:33:43 -0600

    sex inf analysis grandparent
    - cleanup code
    - did data analysis

--- sex_inference_analysis.py
+++ sex_inference_analysis.py
@@ -5,23 +5,54 @@ from typing import TypedDict
 import pandas as pd
 
 from CREST_sex_inference import read_simmap
-from sex_inference_analysis_probabilities import calc_probability_mf
-from utils import read_in_seg_file_as_df, is_beginning_or_end_of_chromosome
+from sex_inference_analysis_probabilities import calc_lod_probability
+from utils import read_in_seg_file_as_df
 
-COLUMN_NAMES = "sample_1", "sample_2", "chromosome", "start", "end", "IBD_type", "genetic_pos_start", "genetic_pos_end", "genetic_len"
-COLUMNS_TO_DROP = ["genetic_pos_start", "genetic_pos_end", "genetic_len"]
-SORT_ORDER = ["sample_1", "sample_2", "chromosome", "start"]
+# ----------------------- utils ----------------------------
 
-column_type_casting = {
-    'chromosome': int,
-    'start': int,
-    'end': int,
-}
+def read_in_seg_file_as_df(seg_file, column_names, columns_to_drop, sort_order, column_type_casting):
+    with open(seg_file) as f:
+        file_lines = f.readlines()
+
+    data = [
+        line.split()
+        for line in file_lines
+    ]
+
+    df = pd.DataFrame(np.array(data), columns=column_names)
+
+    df.drop(columns_to_drop, axis='columns')
+
+    df = df.astype(column_type_casting)
+
+    # make sure the pandas df is sorted to how we want it to be sorted
+    df.sort_values(by=sort_order)
+
+    # check segment end position is greater than segment start position raise exception if not. (CREST does this)
+    for _, segment in df.iterrows():
+        if segment["end"] < segment["start"]:
+            raise Exception("Physical position end less than physical position start")
+
+    return df
 
 
-def get_significant_crossovers_and_gaps(relatives_df, simmap):
+def is_beginning_or_end_of_chromosome(current_row, simmap):
+    def get_chromo_start(chromo, simmap):
+        return simmap[str(chromo)]['pos'][0]
 
-    sig_co_and_gaps = []
+    def get_chromo_end(chromo, simmap):
+        return simmap[str(chromo)]['pos'][-1]
+
+    starts_chromosome = current_row['start'] == get_chromo_start(current_row['chromosome'], simmap)
+    ends_chromosome = current_row['end'] == get_chromo_end(current_row['chromosome'], simmap)
+
+    return starts_chromosome, ends_chromosome
+
+# ----------------------- end utils ----------------------------
+
+def transform_to_co_and_gaps_df(relatives_df, simmap):
+
+    co_and_gaps = []
 
     for index in range(len(relatives_df)):
         curr_row = relatives_df.iloc[index]
@@ -33,7 +64,10 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                               'chromosome': curr_row['chromosome'],
                               'start': curr_row['start'],
                               'end': curr_row['start'],
-                              'is_co': True
+                              'is_co': True,
+                              'breakpoint_overlaps': curr_row['start_overlaps'],
+                              'overlaps_anywhere': curr_row['overlaps_p1'],
+                              'starts_or_ends_chromosome': seg_starts_chromosome
                               }
 
         ending_crossover = {'sample_1': curr_row['sample_1'],
@@ -41,7 +75,10 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                             'chromosome': curr_row['chromosome'],
                             'start': curr_row['end'],
                             'end': curr_row['end'],
-                            'is_co': True
+                            'is_co': True,
+                            'breakpoint_overlaps': curr_row['end_overlaps'],
+                            'overlaps_anywhere': curr_row['overlaps_p1'],
+                            'starts_or_ends_chromosome': seg_ends_chromosome
                             }
 
         seg_gap = {'sample_1': curr_row['sample_1'],
@@ -49,26 +86,27 @@ def get_significant_crossovers_and_gaps(relatives_df, simmap):
                    'chromosome': curr_row['chromosome'],
                    "start": curr_row['start'],
                    "end": curr_row['end'],
-                   'is_co': False
+                   'is_co': False,
+                   'breakpoint_overlaps': False,
+                   'overlaps_anywhere': curr_row['overlaps_p1'],
+                   'starts_or_ends_chromosome': False
                    }
 
         if curr_row['IBD_type'] == 'IBD2':
             continue
 
-        if curr_row['overlaps_p1']:
-            if not seg_starts_chromosome:
-                sig_co_and_gaps.append(starting_crossover)
+        co_and_gaps.append(starting_crossover)
+        co_and_gaps.append(ending_crossover)
+        co_and_gaps.append(seg_gap)
 
-            if not seg_ends_chromosome:
-                sig_co_and_gaps.append(ending_crossover)
-
-            sig_co_and_gaps.append(seg_gap)
-
-    return pd.DataFrame(sig_co_and_gaps)
+    return pd.DataFrame(co_and_gaps)
 
 
 def overlaps(ibd_seg_1, ibd_seg_2):
     return ibd_seg_1['start'] <= ibd_seg_2['end'] and ibd_seg_1['end'] >= ibd_seg_2['start']
+
+def breakpoint_overlaps(breakpoint, other_ibd_seg):
+    return other_ibd_seg['start'] < breakpoint < other_ibd_seg['end']
 
 def seg_overlaps_with_other_ibd_df(seg, other_ibd_df):
     other_ibd_df = other_ibd_df[other_ibd_df['chromosome'] == seg['chromosome']]
@@ -83,9 +121,24 @@ def seg_overlaps_with_other_ibd_df(seg, other_ibd_df):
 
     return False
 
+def breakpoint_overlaps_with_other_ibd_df(breakpoint, chromosome, other_ibd_df):
+    other_ibd_df = other_ibd_df[other_ibd_df['chromosome'] == chromosome]
+
+    for _, other_ibd_seg in other_ibd_df.iterrows():
+        if breakpoint_overlaps(breakpoint, other_ibd_seg):
+            return True
+
+        # assumes other_ibd_df is sorted
+        if breakpoint < other_ibd_seg['start']:
+            break
+
+    return False
+
+
 
 def add_overlap_col_to_ibd_seg_df(ibd_seg_df, ibd_segs_other, overlaps_col_name='overlaps'):
     # sort the rows by chromosome/start/end
+
     ibd_seg_df = ibd_seg_df.sort_values(by=['chromosome', 'start', 'end'], ascending=True)
     ibd_segs_other = ibd_segs_other.sort_values(by=['chromosome', 'start', 'end'], ascending=True)
 
@@ -96,6 +149,14 @@ def add_overlap_col_to_ibd_seg_df(ibd_seg_df, ibd_segs_other, overlaps_col_name=
 
     ibd_seg_df[overlaps_col_name] = ibd_seg_df.apply(
         lambda row: bool(seg_overlaps_with_other_ibd_df(row, ibd_segs_other_chromo_dict[row['chromosome']])), axis=1
+    )
+
+    ibd_seg_df["start_overlaps"] = ibd_seg_df.apply(
+        lambda row: bool(breakpoint_overlaps_with_other_ibd_df(row['start'], row['chromosome'], ibd_segs_other_chromo_dict[row['chromosome']])), axis=1
+    )
+
+    ibd_seg_df["end_overlaps"] = ibd_seg_df.apply(
+        lambda row: bool(breakpoint_overlaps_with_other_ibd_df(row['end'], row['chromosome'], ibd_segs_other_chromo_dict[row['chromosome']])), axis=1
     )
 
     return ibd_seg_df
@@ -133,6 +194,16 @@ def get_pedigree_ids(df):
     return pedigree_ids
 
 
+COLUMN_NAMES = "sample_1", "sample_2", "chromosome", "start", "end", "IBD_type", "genetic_pos_start", "genetic_pos_end", "genetic_len"
+COLUMNS_TO_DROP = ["genetic_pos_start", "genetic_pos_end", "genetic_len"]
+SORT_ORDER = ["sample_1", "sample_2", "chromosome", "start"]
+
+column_type_casting = {
+    'chromosome': int,
+    'start': int,
+    'end': int,
+}
+
 def main(
         input_file,
         map_file,
@@ -153,23 +224,26 @@ def main(
 
         descendants_ids, p1_relatives_ids = determine_descendants_and_relatives(pedigree_id, seg_df)
 
-        pedigree_df = seg_df[seg_df['sample_1'].str.contains(fr'^{pedigree_id}')]
+        pedigree_df = seg_df[seg_df['sample_1'].str.contains(fr'^{pedigree_id}_')]
 
+        # get any ibd between any of the descendants (these are the cousin IBDs)
         descendants_df = get_ibd_segs_between_relatives(pedigree_df, descendants_ids, descendants_ids)
 
+        # get any ibd between any of the descendants and distant relatives (p1 relatives)
         descendants_and_p1_relatives_df = get_ibd_segs_between_relatives(pedigree_df, descendants_ids, p1_relatives_ids)
 
         descendants_df = add_overlap_col_to_ibd_seg_df(descendants_df, descendants_and_p1_relatives_df, overlaps_col_name='overlaps_p1')
 
-        sig_co_and_gaps_df = get_significant_crossovers_and_gaps(descendants_df, simmap=simmap)
+        co_and_gaps_df = transform_to_co_and_gaps_df(descendants_df, simmap=simmap)
 
-        df_results.append(sig_co_and_gaps_df)
+        df_results.append(co_and_gaps_df)
 
         print(pedigree_id)
 
     df_results = pd.concat(df_results, ignore_index=True)
 
     write_df_output(df_results, output_file)
+
 
 class AnalysisParams(TypedDict):
     window: int
@@ -185,37 +259,54 @@ def read_in_results_file_as_df(file):
 def get_pedigree_id_from_file(file):
     return file.split('_')[-1].split(".")[0]
 
-def do_data_analysis(input_file, window_size, output_file, simmap):
-
+def do_data_analysis(input_file, output_file, simmap, details):
     simmap = read_simmap(simmap)
 
-    pedigrees_df = read_in_results_file_as_df(input_file)
-    pedigrees_df = pedigrees_df.astype(column_type_casting)
+    all_pedigrees_df = read_in_results_file_as_df(input_file)
+    all_pedigrees_df = all_pedigrees_df.astype(column_type_casting)
 
-    pedigree_ids = get_pedigree_ids(pedigrees_df)
+    pedigree_ids = get_pedigree_ids(all_pedigrees_df)
+
+    # determine the subset of pedigree ids we want to analyze
+    num_to_analyze = details.get('analysis_count', len(pedigree_ids))
+    female_start = len(pedigree_ids) // 2
+    pedigree_ids = list(pedigree_ids[:num_to_analyze]) + list(pedigree_ids[female_start:female_start+num_to_analyze])
 
     data_analysis_results = []
 
     for pedigree_id in pedigree_ids:
 
-        relative_ids = get_all_relative_ids(pedigree_id, pedigrees_df)
-        pedigree_df = pedigrees_df[pedigrees_df['sample_1'].isin(relative_ids) | pedigrees_df['sample_2'].isin(relative_ids)]
+        relative_ids = get_all_relative_ids(pedigree_id, all_pedigrees_df)
+        single_pedigree_df = all_pedigrees_df[all_pedigrees_df['sample_1'].isin(relative_ids) | all_pedigrees_df['sample_2'].isin(relative_ids)]
 
-        # drop duplicates?
-        # pedigree_df = pedigree_df.drop_duplicates(subset=["chromosome", "start", "end",	"is_co"])
+        # remove any instances where it begins/ends the chromosome (it isn't a crossover we care about)
+        single_pedigree_df = single_pedigree_df[single_pedigree_df['starts_or_ends_chromosome'] == False]
 
-        pedigree_data_analysis_result = {
-            "pedigree_id": pedigree_id,
-        }
+        # only include any ibd co/gaps that overlap with the relatives of the focal grandparent
+        single_pedigree_df = single_pedigree_df[single_pedigree_df['overlaps_anywhere'] == True]
 
-        sig_gaps_df = pedigree_df[pedigree_df['is_co'] == False].to_dict(orient="records")
-        sig_co_df = pedigree_df[pedigree_df['is_co'] == True].to_dict(orient="records")
+        # if indicated, remove any duplicate crossovers
+        if details.get('drop_duplicates', False) == True:
+            single_pedigree_df = single_pedigree_df.drop_duplicates(subset=["chromosome", "start", "end", "is_co"])
 
-        LOD = calc_probability_mf(sig_co_df, sig_gaps_df, simmap, window_size=window_size)
-        pedigree_data_analysis_result[f'LOD'] = LOD
+        # if indicated, only count the crossovers that overlaps a distant relative segment
+        if details.get('only_count_overlapping_ends', False):
+            # drop only those rows where it is a crossover AND it does not directly overlap
+            # keep everything except rows where cond1 is True AND cond2 is False (df = df.loc[(~cond1) | cond2])
+            single_pedigree_df = single_pedigree_df[ ~(single_pedigree_df['overlaps_anywhere']) | (single_pedigree_df['breakpoint_overlaps'])]
 
-        data_analysis_results.append(pedigree_data_analysis_result)
+        # Calculate the score for m/f crossover (higher = more likely female) by adding a column with the LOD
+        single_pedigree_df['LOD'] = single_pedigree_df.apply(calc_lod_probability, axis=1, simmap=simmap, window_size=details.get('window_size', 500))
+
+        LOD = sum(single_pedigree_df['LOD'])
+
+        data_analysis_results.append({'pedigree_id': pedigree_id,
+                                      'LOD': LOD}
+                                     )
+        print(pedigree_id)
 
     results_df = pd.DataFrame(data_analysis_results)
 
     results_df.to_csv(output_file, sep='\t', index=False, header=True)
+
+    return results_df
--- sex_inference_analysis_probabilities.py
+++ sex_inference_analysis_probabilities.py
@@ -20,42 +20,28 @@ def get_co_window(co_position, chromo, window_size, simmap):
     # don't go beyond the usable ends
     return max(co_position - (window_size / 2), gmap_ends[0]), min(co_position + (window_size / 2), gmap_ends[1])
 
-def get_gap_windows(gap, chromo, window_size, simmap):
+def get_gap_windows(gap_start, gap_end, chromo, window_size, simmap):
     gmap = simmap[str(chromo)]
     gmap_ends = (gmap['pos'][0], gmap['pos'][-1])
     # don't go beyond the usable ends
-    return max(gap['start'] + (window_size / 2), gmap_ends[0]), min(gap['end'] - (window_size / 2), gmap_ends[1])
+    return max(gap_start + (window_size / 2), gmap_ends[0]), min(gap_end - (window_size / 2), gmap_ends[1])
 
-def calc_probability(events, simmap, window_size, is_co):
-    list_logp_female = []
-    list_logp_male = []
+def calc_lod_probability(event, simmap, window_size):
+    is_co = event['is_co']
+    if is_co:
+        adjusted_start, adjusted_end = get_co_window(event['start'], event["chromosome"], window_size, simmap)
+        num_events = 1
+    else:
+        adjusted_start, adjusted_end = get_gap_windows(event['start'], event['end'], event["chromosome"], window_size, simmap)
+        num_events = 0
 
-    for event in events:
-        if is_co:
-            adjusted_start, adjusted_end = get_co_window(event['start'], event["chromosome"], window_size, simmap)
-            num_events = 1
-        else:
-            adjusted_start, adjusted_end = get_gap_windows(event, event["chromosome"], window_size, simmap)
-            num_events = 0
+    if adjusted_start > adjusted_end:
+        print("The start is greater than the end of the segment")
+        return 0
 
-        if adjusted_start > adjusted_end:
-            print("The start is greater than the end of the segment")
-            continue
+    lengths = get_mf_lengths(event['chromosome'], adjusted_start, adjusted_end, simmap)
 
-        lengths = get_mf_lengths(event['chromosome'], adjusted_start, adjusted_end, simmap)
+    female_logp = log_poisson(num_events, lengths['female'])
+    male_logp = log_poisson(num_events, lengths['male'])
 
-        list_logp_female.append(log_poisson(num_events, lengths['female']))
-        list_logp_male.append(log_poisson(num_events, lengths['male']))
-
-    return list_logp_female, list_logp_male
-
-def calc_probability_mf(significant_crossovers, significant_gaps, simmap, window_size):
-    co_list_logp_female, co_list_logp_male = calc_probability(significant_crossovers, simmap, window_size, is_co=True)
-    gap_list_logp_female, gap_list_logp_male = calc_probability(significant_gaps, simmap, window_size, is_co=False)
-
-    logp_female = log_product(co_list_logp_female + gap_list_logp_female + gap_list_logp_female)
-    logp_male = log_product(co_list_logp_male + gap_list_logp_male + gap_list_logp_male)
-
-    LOD_all = logp_female - logp_male
-
-    return LOD_all
+    return (female_logp - male_logp)
